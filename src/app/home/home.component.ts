124808a0886fc62785d2c4bd62bf1bd2d85d0557
deepali-arvind <deepali-arvind>
2018-06-12 05:25:30 +0000

    Initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,9 @@ import {Router} from "@angular/router";
   templateUrl: 'home.html'
 })
 export class HomeComponent {
-   menuData:any;	
   constructor(private _router : Router){
-    
- this.menuData=[{"text":"Core","expand":true,"children":[{"text":"Tempui","routerLink":"core/tempui","selected":false}]}];   }
+
+  }
   routeToLink(data: any){
      if(!data.hasOwnProperty('children') && data.routerLink)
       this._router.navigate([data.routerLink]);
